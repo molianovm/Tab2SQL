4e23e04adb3246ee7ed821456a1b88536334425b
Molianov Maksim <molianov.maksim@yandex.ru>
2025-02-15 16:49:44 +0300

    Init gui app

---
+++ app/gui/main_window.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from tkinter import ttk, filedialog, messagebox
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Tab2SQL")
+        self.geometry("1200x600")
---
+++ app/main.py
@@ -0,0 +1,9 @@
+# main.py
+from gui.main_window import App
+
+def main():
+    app = App()
+    app.mainloop()
+
+if __name__ == "__main__":
+    main()
